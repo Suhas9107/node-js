08e0adef8b421d724c1721c44db43dbd47837c21
Suhas <ssuhas292@gmail.com>
2023-02-14 10:56:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Image from "./Assets/470a19a36904fe200610cc1f41eb00d9.jpg"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+  const x = 10;
+  var text = "Not equal";
+  if (x===10){
+    text = "Equal";
+  }else {
+    text = "Not Equal";
+  }
+return( 
+<div className="App">
+<h1>Hello World</h1>
+<p>This is paragraph</p>
+<h3>{text}</h3>
+<h3>{5+30}</h3>
+<img src={Image} alt="pic" width="50%"  /> 
+<h4>{x > 10? "greater" : "lesser"}</h4>
+</div>
+);
 }
-
 export default App;
